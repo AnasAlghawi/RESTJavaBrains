9464586ad0e72c61a3c7f49dd6bb6abe5f44ce24
Anas Alghawi <anas.alghawi@gmail.com>
2017-08-19 14:45:48 +0300

    firts

---
+++ messenger/src/webservice/recources/MessageResource.java
@@ -0,0 +1,13 @@
+package webservice.recources;
+
+public class MessageResource {
+    
+    public String getMessages(){
+        
+        return "HelloWorld";
+        
+        }
+                      
+                      
+                     
+}
